ce93d62f9c7f4ad71e7c7ddc90d5028235c2d2d7
dogkeeper886 <jack.w.tseng@outlook.com>
2020-06-19 14:14:11 +0800

    update

--- ap-set-hub.py
+++ ap-set-hub.py
@@ -13,7 +13,13 @@ print('apIp:', apIp)
 # start ap conect
 con = 'ssh ' + apIp
 child = pexpect.spawn(con, encoding='utf-8', logfile=sys.stdout)
-child.expect('user name')
+
+# login ap
+child.expect('Please login:')
+child.send('super')
+child.expect('password :')
+child.send('sp-admin')
+child.expect('rkscli:')
 
 # end ap connect
 if child.isalive():
