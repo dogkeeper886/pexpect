9ef8592a85243a3e0c931283ee71937991edeeeb
dogkeeper886 <jack.w.tseng@outlook.com>
2020-06-30 10:05:52 +0800

    fix syntax

--- setup-vsz-network.py
+++ setup-vsz-network.py
@@ -30,7 +30,7 @@ child.expect('#')
 
 # setup vsz profile
 child.sendline('setup')
-child.expect('Select vSZ Profile (1/2):')
+child.expect('Select vSZ Profile \(1/2\):')
 if vszProfile in ['E', 'e']:
     child.sendline('1')
 if vszProfile in ['H', 'h']:
@@ -38,17 +38,17 @@ if vszProfile in ['H', 'h']:
 child.sendline('Y')
 
 # setup vsz ip address
-child.expect('Select address type: (1/2)')
+child.expect('Select address type: \(1/2\)')
 child.sendline('1')
-child.expect('Select IP configuration: (1/2)')
+child.expect('Select IP configuration: \(1/2\)')
 child.sendline('2')
 child.expect('Primary DNS:')
 child.sendline('10.10.10.10')
 child.expect('Secondary DNS:')
 child.sendline('10.10.10.106')
-child.expect('Do you want to apply the settings? (y/n)')
+child.expect('Do you want to apply the settings? \(y/n\)')
 child.sendline('y')
-
+child.expect('#')
 
 # log out
 child.sendline('exit')
